b5fb414888ba726533ea089ef8eb7c063c1233a7
ZhiFang Tan <zftan0709@gmail.com>
2019-07-11 13:58:40 -0500

    Modified codec to mpeg.

--- video.py
+++ video.py
@@ -27,16 +27,16 @@ def main():
     image_list = [image_file for image_file in image_list if os.path.splitext(image_file)[1][1:].lower() in IMAGE_EXT]
 
     #two methods of naming output video to handle varying environemnts
-    video_file_1 = args.image_folder + '.mp4'
-    video_file_2 = args.image_folder + 'output_video.mp4'
+    video_file_1 = args.image_folder + '.avi'
+    video_file_2 = args.image_folder + 'output_video.avi'
 
     print("Creating video {}, FPS={}".format(args.image_folder, args.fps))
     clip = ImageSequenceClip(image_list, fps=args.fps)
     
     try:
-        clip.write_videofile(video_file_1)
+        clip.write_videofile(video_file_1,codec='mpeg4')
     except:
-        clip.write_videofile(video_file_2)
+        clip.write_videofile(video_file_2,codec='mpeg4')
 
 
 if __name__ == '__main__':
